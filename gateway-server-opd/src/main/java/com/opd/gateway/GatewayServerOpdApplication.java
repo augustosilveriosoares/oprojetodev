72c1b0278b1ace955ff01b19737d3d2f8d2c2abf
augustosilveriosoares <falecomsilverio@gmail.com>
2024-09-15 22:55:20 -0300

    Adicionando Gateway Server

---
+++ gateway-server-opd/src/main/java/com/opd/gateway/GatewayServerOpdApplication.java
@@ -0,0 +1,13 @@
+package com.opd.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServerOpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerOpdApplication.class, args);
+	}
+
+}
---
+++ gateway-server-opd/src/test/java/com/opd/gateway/GatewayServerOpdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opd.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerOpdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
