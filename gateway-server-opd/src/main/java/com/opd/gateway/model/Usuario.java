4913678ad36582cd5f02c6f8a23756da923cb272
augustosilveriosoares <falecomsilverio@gmail.com>
2024-09-29 22:05:11 -0300

    Adicionando Objetos de usuario para autenticacao

---
+++ gateway-server-opd/src/main/java/com/opd/gateway/model/Usuario.java
@@ -0,0 +1,76 @@
+package com.opd.gateway.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+	@Id
+	private String id;
+	private String email;
+	private String nome;
+	private String senha;
+
+	public Usuario() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
